b9d1d4bbeb686182c8c078a56a517ec5661dab3d
juzhao_indra <168105725+juzha-minsait@users.noreply.github.com>
2024-05-09 17:56:52 +0200

    Create 5_About_Us.py

---
+++ pages/5_About_Us.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Gracias")
+
+st.markdown("##### Gracias por su atención. Esperamos que haya disfrutado de la presentación.")
+
+st.markdown("##### Junyi Zhao")
+st.markdown("##### Hongwen Li")
+st.markdown("##### Liu Cai")
